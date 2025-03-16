0c6752b84a7797bb608063129b2890ca5284de81
Tarık Çekirge <tcekirge@gmail.com>
2025-03-16 17:15:00 +0300

    Adding a Background Image

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <>
-      <StartGameScreen />
+    <LinearGradient
+      colors={["#7F0541", "#4E0328", "#7F0541"]}
+      locations={[0.1, 0.5, 1]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+      style={styles.appContainer}
+    >
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode='cover'
+        style={styles.appContainer}
+        imageStyle={styles.bgImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+
       <StatusBar style='auto' />
-    </>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  bgImage: {
+    opacity: 0.5,
+  },
+});
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,8 +12,14 @@ const StartGameScreen = () => {
         autoCapitalize='none'
         autoCorrect={false}
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
@@ -22,6 +28,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
     padding: 16,
     marginTop: 100,
     backgroundColor: "#67063c",
@@ -42,6 +50,11 @@ const styles = StyleSheet.create({
     color: "#ddb52f",
     marginVertical: 8,
     textAlign: "center",
-    marginHorizontal: "auto",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
   },
 });
